c4e4fdc9e1fc1c10d4dbb3b3779d9b84a0d11a36
m0a <m0a@abe-mac.local>
2014-07-06 19:07:06 +0900

    Initial Commit

---
+++ CurrencyConverter/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CurrencyConverter
+//
+//  Created by m0a on 2014/07/06.
+//  Copyright (c) 2014年 m0a. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet var window: NSWindow
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ CurrencyConverter/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  CurrencyConverter
+//
+//  Created by m0a on 2014/07/06.
+//  Copyright (c) 2014年 m0a. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
